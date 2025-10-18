f1c215777db2bb7081cdf907f205dff9c750c237
Sahil Suman <sahilsuman1202@gmail.com>
2025-10-18 20:01:09 +0530

    Added Testimonial section & Navbar in Layout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,8 +11,8 @@ const Navbar = () => {
 
   return (
     <div className='fixed z-50 w-full backdrop-blur-2xl flex justify-between
-    items-center py-[-1] px-4 sm:px-20 xl:px-32 cursor-pointer'>
-        <img src={assets.logo} alt='logo' className='w-42 sm:w-44 cursor-pointer' onClick={() => {
+    items-center py-[-1] px-4 sm:px-20 xl:px-32'>
+        <img src={assets.logo} alt='logo' className='w-32 sm:w-44 cursor-pointer' onClick={() => {
             navigate('/')
         }}/>
 
