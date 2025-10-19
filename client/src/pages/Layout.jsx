48fb6c220ae8f2d7dad4f4707ccce819cd75d66c
Sahil Suman <sahilsuman1202@gmail.com>
2025-10-19 14:37:59 +0530

    Completed Dashboard

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -2,13 +2,16 @@ import React, {useState} from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
 import {Menu, X} from 'lucide-react'
+import { useUser, SignIn } from '@clerk/clerk-react';
+import Sidebar from '../components/Sidebar';
 
 const Layout = () => {
 
   const navigate = useNavigate()
   const [sidebar, setSidebar] = useState(false)
+  const {user} = useUser();
   
-  return (
+  return user ? (
     <div className='flex flex-col items-start justify-start h-screen'>
 
       <nav className='w-full px-8 min-h-14 flex items-center justify-between 
@@ -19,8 +22,18 @@ const Layout = () => {
           : <Menu onClick={() => setSidebar(true)} className='w-6 h-6 text-gray-600 sm:hidden'/>
         }
       </nav>
-        
+      <div className='flex-1 w-full flex h-[calc(100vh-64px)]'>
+        <Sidebar sidebar={sidebar} setSidebar={setSidebar}/>        
+        <div className='flex-1 overflow-auto'>
         <Outlet/>
+        </div>
+      </div>
+        
+      
+    </div>
+  ) : (
+    <div className='flex items-center justify-center h-screen'>
+      <SignIn />
     </div>
   )
 }
