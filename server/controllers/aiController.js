05828f1105a4a0521e2fc76477bdc2e142e25774
Sahil Suman <sahilsuman1202@gmail.com>
2025-10-24 14:38:17 +0530

    Added Middleware

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,4 +1,11 @@
+import OpenAI from "openai";
+import sql from "../configs/db.js";
+import { clerkClient } from "@clerk/express";
 
+const AI = new OpenAI({
+    apiKey: "GEMINI_API_KEY",
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
+});
 
 export const generateArticle = async () => {
     try{
@@ -11,9 +18,36 @@ export const generateArticle = async () => {
             return resizeBy.json({success: false, message: "Limit reached. Upgrade to continue."})
         }
         // AI Logic here
-        
+        const response = await AI.chat.completions.create({
+        model: "gemini-2.0-flash",
+        messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        {
+            role: "user",
+            content: "prompt",
+        },
+    ],
+            temperature: 0.7,
+            max_tokens: length,
+});
+        const content = response.choices[0].message.content
 
-    } catch(error) {
+        await sql` INSERT INTO creations (user_id, prompt, content, type)
+        VALUES (${userId}, ${prompt}, ${content}, 'article') `;
 
+        if(plan !== 'premium'){
+            await clerkClient.users,updateUseerMetadata(userId, {
+                privateMetadata :{
+                    free_usage: free_usage + 1
+                }
+        })
+
+} 
+
+response.json({success: true, content})
+
+} catch(error) {
+    console.log(error.message)
+    res.json({success: false, message: error.message})
     }
 }
