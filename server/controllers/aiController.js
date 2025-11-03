12e64d58689c012cd76574b540fef625cf2a4377
Sahil Suman <sahilsuman1202@gmail.com>
2025-11-03 23:36:35 +0530

    feat: implement Cloudinary integration for image generation and update routes for new functionalities

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,6 +1,9 @@
 import OpenAI from "openai";
 import sql from "../configs/db.js";
 import { clerkClient } from "@clerk/express";
+import { response } from "express";
+import {v2 as cloudinary} from 'cloudinary';
+import axios from "axios";
 
 const AI = new OpenAI({
     apiKey: process.env.GEMINI_API_KEY,
@@ -110,26 +113,21 @@ export const generateImage = async (req, res) => {
             return res.json({success: false, message: " This feature is only available for premium subscriptions."})
         }
         // AI Logic here 
-        const response = await AI.chat.completions.create({
-        model: "gemini-2.0-flash",
-        messages: [{role: "user", content: prompt,},],
-            temperature: 0.7,
-            max_tokens: 100,
-    });
-        const content = response.choices[0].message.content
+        const formData = new FormData()
+        formData.append('prompt', prompt)
+        const {data} = await axios.post("https://clipdrop-api.co/text-to-image/v1", formData, {
+            Headers: {'x-api-key': process.env.CLIPDROP_API_KEY,},
+            responseType: 'arraybuffer',
+        })
 
-        await sql` INSERT INTO creations (user_id, prompt, content, type)
-        VALUES (${userId}, ${prompt}, ${content}, 'blog-title') `;
+        const base64Image = `data:image/png;base64,${Buffer.from(data, 'binary').toString('base64')}`;
 
-        if(plan !== 'premium'){
-            await clerkClient.users.updateUserMetadata(userId, {
-                privateMetadata: {
-                    free_usage: free_usage + 1
-                }
-            })
-} 
+        const {secure_url} = await cloudinary.uploader.upload(base64Image)
 
-res.json({success: true, content})
+        await sql` INSERT INTO creations (user_id, prompt, content, type, publish)
+        VALUES (${userId}, ${prompt}, ${content}, 'image', ${publish ?? false}) `;
+
+res.json({success: true, content: secure_url})
 
 } catch(error) {
     console.log(error.message)
