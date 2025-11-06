e4ebffb28b78887416836cd093eb60dfbaff5977
Sahil Suman <sahilsuman1202@gmail.com>
2025-11-06 14:24:40 +0530

    refactor: clean up getUserCreations and ensure consistent response structure in getPublishedCreations and toggleLikeCreations

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,7 +7,31 @@ export const getUserCreations = async (req, res) => {
         // creation dhundho user ki
         const creations = await sql`SELECT * FROM creations WHERE user_id = ${userId} ORDER BY created_At DESC`;
 
-        
+        res.json({success:true, creations});
+
+    } catch(error){
+        res.json({success: false, message: error.message});
+    }
+}
+
+export const getPublishedCreations = async (req, res) => {
+    try{
+        const creations = await sql`
+        SELECT * FROM creations WHERE publish = true ORDER BY created_At DESC`;
+
+        res.json({success:true, creations});
+
+    } catch(error){
+        res.json({success: false, message: error.message});
+    }
+}
+
+export const toggleLikeCreations = async (req, res) => {
+    try{
+        const creations = await sql`
+        SELECT * FROM creations WHERE publish = true ORDER BY created_At DESC`;
+
+        res.json({success:true, creations});
 
     } catch(error){
         res.json({success: false, message: error.message});
